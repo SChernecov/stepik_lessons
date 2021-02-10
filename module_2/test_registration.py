81c19ad0bde93d0f5773891dfd93543c616a83e4
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-02-10 03:40:30 +0300

    create test_registration.py

--- module_2/test_registration.py
+++ module_2/test_registration.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from sys import argv
-import time
 
+# Задаем в скрипте параметры
 script_name, link = argv
 
 try:
@@ -9,16 +9,18 @@ try:
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
-    ...
+    first_name = browser.find_element_by_css_selector("[placeholder='Input your first name']")
+    first_name.send_keys("тест")
+    last_name = browser.find_element_by_css_selector("[placeholder='Input your last name']")
+    last_name.send_keys("тест")
+    email = browser.find_element_by_css_selector("[placeholder='Input your email']")
+    email.send_keys("тест")
 
     # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
     # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
-
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element_by_tag_name("h1")
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
@@ -28,9 +30,5 @@ try:
     assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
     # закрываем браузер после всех манипуляций
     browser.quit()
-
-# правильная команда для запуска с ключом - python module_2/test_registration.py http://suninjuly.github.io/registration1.html
