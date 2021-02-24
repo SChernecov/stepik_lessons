a47f1e2240b6d156b4eded2a9c63c2fc2e3423d9
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-02-25 01:53:08 +0300

    create test_param.py.py

--- module_4/test_param.py
+++ module_4/test_param.py
@@ -1,7 +1,10 @@
-from selenium import webdriver
-import pytest
 import time
+import pytest
 import math
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
 answer = math.log(int(time.time()))
 
@@ -13,17 +16,21 @@ def browser():
     browser.quit()
 
 
-@pytest.parametrize('lessons', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
-def test_openned_lessons(browser, lessons):
+@pytest.mark.parametrize('lessons', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+def test_open_lessons(browser, lessons):
     link = f"https://stepik.org/lesson/{lessons}/step/1"
     browser.get(link)
 
-    input_file = browser.find_element_by_css_selector('.ember-text-area')
+    input_file = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, '.ember-text-area')))
     input_file.send_keys(str(answer))
 
-    send_button = browser.find_element_by_css_selector('.submit-submission')
+    send_button = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '.submit-submission')))
     send_button.click()
 
-    feedback_field = browser.find_element_by_css_selector('.smart-hints__hint')
+    feedback_field = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint')))
     feedback_field_text = feedback_field.text
     assert feedback_field_text == 'Correct', 'Feedback field is incorrect!'
