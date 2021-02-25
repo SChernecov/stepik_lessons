c3a79f334dddd433332a301ed470f8ee49ed2e14
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-02-26 01:28:33 +0300

    refactoring test_items

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,31 +1,12 @@
-import conftest.py
-
-main_page_link = "http://selenium1py.pythonanywhere.com/ru/"
-
-book_link_locator = "[title='Coders at Work']"
-all_goods_locator = "[href='\/ru\/catalogue\/']"
-add_book_button_locator = ".btn-primary"
-
-
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
 def test_add_book_button_ru(browser):
-    browser.get(main_page_link)
-    all_goods = browser.find_element_by_css_selector(all_goods_locator)
-    all_goods.click()
-    book_link = browser.find_element_by_css_selector(book_link_locator)
-    book_link.click()
+    # Data
+    book_page_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    add_book_button_locator = ".btn-primary"
+    value_book_button_attribut = "value"
+    # Act
+    browser.get(book_page_link)
+    # Assert
     add_book_button = browser.find_element_by_css_selector(add_book_button_locator)
     add_book_button_text = add_book_button.text
-    assert add_book_button_text == 'Добавить в корзину', 'Не отображается кнопка "Добавить в корзину"'
-
-
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_add_book_button_com(browser):
-    browser.get(main_page_link)
-    all_goods = browser.find_element_by_css_selector(all_goods_locator)
-    all_goods.click()
-    book_link = browser.find_element_by_css_selector(book_link_locator)
-    book_link.click()
-    add_book_button = browser.find_element_by_css_selector(add_book_button_locator)
-    add_book_button_text = add_book_button.text
-    assert add_book_button_text == 'Добавить в корзину', 'Не отображается кнопка "Добавить в корзину"'
+    value_book_button = add_book_button.get_attribute(value_book_button_attribut)
+    assert add_book_button_text == value_book_button, 'Не отображается кнопка "Добавить в корзину"'
