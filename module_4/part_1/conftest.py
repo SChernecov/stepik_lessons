3188b605a2e3ea6ced22c180871316f88f98ef82
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-02-27 14:55:08 +0300

    refactoring conftest.py and test_items.py

--- module_4/part_1/conftest.py
+++ module_4/part_1/conftest.py
@@ -1,15 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="None", help="Choose language: ru, en-GB, es, fr")
+
+@pytest.fixture
+def language(request):
+    return request.config.getoption("--language")
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(language):
+    # Задаем опции для запуска драйвера браузера
+    options = Options()
+    # Добавляем опцию intl.accept_languages
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    # Запускаем браузер Chrome и говорим чтобы использовал опции, которые мы опеределили выше
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
-
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,12 +1,19 @@
-def test_add_book_button_ru(browser):
+def test_add_book_button_ru(browser, language):
     # Data
     book_page_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     add_book_button_locator = ".btn-primary"
-    value_book_button_attribut = "value"
     # Act
     browser.get(book_page_link)
     # Assert
     add_book_button = browser.find_element_by_css_selector(add_book_button_locator)
     add_book_button_text = add_book_button.text
-    value_book_button = add_book_button.get_attribute(value_book_button_attribut)
-    assert add_book_button_text == value_book_button, 'Не отображается кнопка "Добавить в корзину"'
+    if language == "ru":
+        assert add_book_button_text == 'Добавить в корзину', 'Не отображается текст кнопки "Добавить в корзину"'
+    elif language == "en-GB":
+        assert add_book_button_text == 'Add to basket', 'Не отображается текст кнопки "Add to basket"'
+    elif language == "es":
+        assert add_book_button_text == 'Añadir al carrito', 'Не отображается текст кнопки "Añadir al carrito"'
+    elif language == "fr":
+        assert add_book_button_text == 'Ajouter au panier', 'Не отображается текст кнопки "Ajouter au panier"'
+    else:
+        pytest.skip(f"Unsupported language: {language}")
