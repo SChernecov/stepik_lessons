f172331af43ecf1f6ef84785ef4525500a408d5c
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-02 23:58:53 +0300

    refactoring test_items.py

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,22 +1,15 @@
-import pytest
-
 add_book_button_locator = ".btn-primary"
+
 
 def test_add_book_button_ru(browser, language):
     # Data
     book_page_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    button_values_options = {"ru": 'Добавить в корзину', "en-GB": 'Add to basket', "es": 'Añadir al carrito',
+                             "fr": 'Ajouter au panier'}
+
     # Act
     browser.get(book_page_link)
     # Assert
     add_book_button = browser.find_element_by_css_selector(add_book_button_locator)
     add_book_button_text = add_book_button.text
-    if language == "ru":
-        assert add_book_button_text == 'Добавить в корзину', 'Не отображается текст кнопки "Добавить в корзину"'
-    elif language == "en-GB":
-        assert add_book_button_text == 'Add to basket', 'Не отображается текст кнопки "Add to basket"'
-    elif language == "es":
-        assert add_book_button_text == 'Añadir al carrito', 'Не отображается текст кнопки "Añadir al carrito"'
-    elif language == "fr":
-        assert add_book_button_text == 'Ajouter au panier', 'Не отображается текст кнопки "Ajouter au panier"'
-    else:
-        pytest.skip(f"Язык не поддерживается: {language}")
+    assert add_book_button_text == button_values_options[language]
