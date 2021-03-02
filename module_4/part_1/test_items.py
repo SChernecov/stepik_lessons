cecdc75a1ca51bf065e2e17e851809085fcbd331
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-03 00:04:40 +0300

    refactoring test_items.py

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -9,7 +9,7 @@ def test_add_book_button_ru(browser, language):
 
     # Act
     browser.get(book_page_link)
-    # Assert
     add_book_button = browser.find_element_by_css_selector(add_book_button_locator)
     add_book_button_text = add_book_button.text
+    # Assert
     assert add_book_button_text == button_values_options[language]
