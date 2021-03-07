6669b2608597232453b55e0c71f86de0ddec8e8d
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-07 22:54:30 +0300

    create conftest.py in module 5

---
+++ module_5/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="None", help="Choose language: ru, en-GB, es, fr")
+
+@pytest.fixture
+def language(request):
+    return request.config.getoption("--language")
+
+@pytest.fixture(scope="function")
+def browser(language):
+    # Задаем опции для запуска драйвера браузера
+    options = Options()
+    # Добавляем опцию intl.accept_languages
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    # Запускаем браузер Chrome и говорим чтобы использовал опции, которые мы опеределили выше
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
