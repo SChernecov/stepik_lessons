85dc97342a7fdd0ae56a1d4c0b98954d35a25c11
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-11 15:34:33 +0300

    some refactoring

--- module_5/pages/locators.py
+++ module_5/pages/locators.py
@@ -10,3 +10,10 @@ class LoginPageLocators():
     REGISTRATION_EMAIL_LOCATOR = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTRATION_PASSWORD_LOCATOR = (By.CSS_SELECTOR, "#id_registration-password1")
     REPEAT_REGISTRATION_EMAIL_LOCATOR = (By.CSS_SELECTOR, "#id_registration-password2")
+
+class BasketPageLocators():
+    ADD_BOOK_BUTTON_LOCATOR = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME_LOCATOR = (By.CSS_SELECTOR, "article h1")
+    ADD_BOOK_NAME_LOCATOR = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    BOOK_PRICE_LOCATOR = (By.CSS_SELECTOR, "p.price_color")
+    MESSAGE_BASKET_PRICE_LOCATOR = (By.CSS_SELECTOR, "#messages > div > div > p:nth-child(1) > strong")
