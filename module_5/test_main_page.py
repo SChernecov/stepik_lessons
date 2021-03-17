2d682f211e9ac7bec024719981ed4099e8958b80
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-17 20:40:01 +0300

    По указанию преподавателя применил правило AAA

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -8,20 +8,35 @@ link = "http://selenium1py.pythonanywhere.com/"
 @pytest.mark.login_guest
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
+        # Arrange
         page = MainPage(browser, link)
         page.open()
+
+        # Act
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
+
+        # Assert
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
+        # Arrange
         page = MainPage(browser, link)
+
+        # Act
         page.open()
+
+        # Assert
         page.should_be_login_link()
 
     def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        # Arrange
         page = BasketPage(browser, link)
         page.open()
+
+        # Act
         page.go_to_basket_page()
+
+        # Assert
         page.should_not_be_products_in_basket()
         page.should_be_message_empty_basket()
