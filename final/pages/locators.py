bcf77f755db755751124357f0c988d96172c6b47
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-19 23:18:40 +0300

    Подготовка к сдаче финального задания

---
+++ final/pages/locators.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK_LOCATOR = (By.CSS_SELECTOR, "span .btn-default:nth-child(1)")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class LoginPageLocators:
+    LOGIN_EMAIL_LOCATOR = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD_LOCATOR = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTRATION_EMAIL_LOCATOR = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD_LOCATOR = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_BUTTON_LOCATOR = (By.CSS_SELECTOR, "button[name='registration_submit']")
+    REPEAT_REGISTRATION_EMAIL_LOCATOR = (By.CSS_SELECTOR, "#id_registration-password2")
+
+
+class ProductPageLocators:
+    ADD_PRODUCT_BUTTON_LOCATOR = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME_LOCATOR = (By.CSS_SELECTOR, ".product_main > h1")
+    PRODUCT_NAME_IN_MESSAGE_LOCATOR = (By.CSS_SELECTOR, "#messages > div:nth-child(1) strong")
+    PRODUCT_PRICE_LOCATOR = (By.CSS_SELECTOR, "p.price_color")
+    BASKET_PRICE_IN_MESSAGE_LOCATOR = (By.CSS_SELECTOR, ".alertinner > p > strong")
+    SUCCESS_MESSAGE_LOCATOR = (By.CSS_SELECTOR, "#messages div:nth-of-type(1) .alertinner")
+
+
+class BasketPageLocators:
+    BASKET_MESSAGE_LOCATOR = (By.CSS_SELECTOR, "#content_inner > p")
+    BASKET_PRODUCTS_LOCATOR = (By.CSS_SELECTOR, "#basket_formset")
---
+++ final/test_main_page.py
@@ -0,0 +1,41 @@
+from module_5.pages.main_page import MainPage
+from module_5.pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+class TestMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        # Arrange
+        page = MainPage(browser, link)
+        page.open()
+
+        # Act
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+
+        # Assert
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        # Arrange
+        page = MainPage(browser, link)
+
+        # Act
+        page.open()
+
+        # Assert
+        page.should_be_login_link()
+
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        # Arrange
+        page = BasketPage(browser, link)
+        page.open()
+
+        # Act
+        page.go_to_basket_page()
+
+        # Assert
+        page.should_not_be_products_in_basket()
+        page.should_be_message_empty_basket()
