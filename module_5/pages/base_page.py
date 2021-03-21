a2952c7698a2be00128dd627fea226ca24450898
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-21 16:59:50 +0300

    Финальные правки перед тз

--- module_5/pages/base_page.py
+++ module_5/pages/base_page.py
@@ -38,7 +38,7 @@ class BasePage():
 
     def should_be_authorized_user(self):
         assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
-                                                                     " probably unauthorised user"
+                                                                     "probably unauthorised user"
 
     def is_element_present(self, how, what):
         try:
