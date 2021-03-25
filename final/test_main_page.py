a58b4a267e821e049cbfc6efe753b48604bb80fc
SChernecov <65087938+SChernecov@users.noreply.github.com>
2021-03-25 22:05:22 +0300

    По указанию преподавателя оставил только персональные тесты

--- final/test_main_page.py
+++ final/test_main_page.py
@@ -1,47 +1,11 @@
 import pytest
 
-from final.pages.main_page import MainPage
-from final.pages.basket_page import BasketPage
 from final.pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
 class TestMainPage:
-    def test_guest_can_go_to_login_page(self, browser):
-        # Arrange
-        page = MainPage(browser, link)
-        page.open()
-
-        # Act
-        page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
-
-        # Assert
-        login_page.should_be_login_page()
-
-    def test_guest_should_see_login_link(self, browser):
-        # Arrange
-        page = MainPage(browser, link)
-
-        # Act
-        page.open()
-
-        # Assert
-        page.should_be_login_link()
-
-    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
-        # Arrange
-        page = BasketPage(browser, link)
-        page.open()
-
-        # Act
-        page.go_to_basket_page()
-
-        # Assert
-        page.should_not_be_products_in_basket()
-        page.should_be_message_empty_basket()
-
     @pytest.mark.personal_test
     def test_guest_can_log_in_with_correct_data(self, browser):
         # Arrange
